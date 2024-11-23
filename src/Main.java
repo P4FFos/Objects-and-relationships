7e1a2575ff1fe36b1b29056e6e35a235e02e78ec
Danis Music <danis200004@icloud.com>
2024-11-23 14:52:25 +0100

    Add example employee to showcase some of the methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Employee em1 = new Employee("ID1", "Bob", 69420);
+
+        System.out.println(em1.toString());
+        System.out.println(em1.getNetSalary());
+        System.out.println(Util.truncate(em1.getNetSalary()));
     }
 }
